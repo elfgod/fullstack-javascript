7b0208cbeec0cb3459c9faf7979d029f6e4d80cf
ElfGod <albertogx@icloud.com>
2023-01-16 23:50:56 -0500

    Component children part1

---
+++ 04-website/src/App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import PageWrapper from './Components/PageWrapper';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from './Components/Pages/Home';
+import About from './Components/Pages/About';
+import Contact from './Components/Pages/Contact';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <PageWrapper>
+            <Route
+              exact={true}
+              path="/"
+              component={Home}
+            />
+            <Route
+              path="/about"
+              component={About}
+            />
+            <Route
+              path="/contact"
+              component={Contact}
+            />
+        </PageWrapper>
+      </Router>
+    );
+  }
+}
+
+export default App;
